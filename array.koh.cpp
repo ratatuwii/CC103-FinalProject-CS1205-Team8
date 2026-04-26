9af2586ab291e33db36248863d3e7d24614c3142
kelsey <kelseyleighmc@gmail.com>
2026-04-26 10:46:07 +0800

    di ko sure if tama yan nag browser lang ako, pero eto yung mga recent edits ko sa array.koh.cpp. Sana makatulong sa project natin!

--- array.koh.cpp
+++ array.koh.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
 using namespace std;
+
+class order{
+  private:
+  string name;
+  string details;
+  int priority;
+
+  public:
+  order (string n, string d, int p){
+    name = n;
+    details = d;
+    priority = p;
+  }
+  
+}
 
 int main(){
 
